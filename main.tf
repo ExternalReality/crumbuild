b11014991e8d8566db6c43ff182118e0f8437977
externalreality <ejones@protonmail.com>
2021-04-29 13:57:50 -0400

    Do run.

--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ variable "region" {
 }
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region  = "${var.region}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region  = var.region
 }
 
 resource "aws_instance" "Demo" {
