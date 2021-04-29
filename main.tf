35b339c7e070372fdea172f0b909be24fb142f2f
externalreality <ejones@protonmail.com>
2021-04-29 12:40:24 -0400

    ec2 instance

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 provider "aws" {
-  version = "~> 3.0"
-  region  = var.region
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "Demo" {
+  ami           = "ami-0d758c1134823146a"
+  instance_type = "t2.micro"
+  key_name = "key_pair_name"
+  tags = {
+    Name = "Demo"
+  }
 }
