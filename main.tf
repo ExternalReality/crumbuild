2c018f6626a201271bd8a1831c85e17326d7b5dd
externalreality <ejones@protonmail.com>
2021-04-29 13:33:58 -0400

    vars

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+variable "aws_access_key" {
+  type = string
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type = string
+  sensitive = true
+}
+
+variable "region" {
+        default = "us-east-1"
+}
+
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
@@ -10,12 +24,4 @@ resource "aws_instance" "Demo" {
   tags = {
     Name = "Demo"
   }
-}
-
-variable "aws_access_key" {}
-
-variable "aws_secret_key" {}
-
-variable "region" {
-        default = "us-east-1"
 }
